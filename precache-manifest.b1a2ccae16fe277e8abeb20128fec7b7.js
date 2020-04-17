f329d9e3a92026e4ce46ec6ca83d7c0346a5a788
yadev2019 <gudeverloper@gmail.com>
2020-04-18 00:15:50 +0700

    Updates

---
+++ precache-manifest.b1a2ccae16fe277e8abeb20128fec7b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03e33c1fd6b748834e89f8fefcd777aa",
+    "url": "/cient-app/index.html"
+  },
+  {
+    "revision": "dc77ab799393b0bec678",
+    "url": "/cient-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4e8a669bb84bf0de1da1",
+    "url": "/cient-app/static/js/2.643e86f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cient-app/static/js/2.643e86f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc77ab799393b0bec678",
+    "url": "/cient-app/static/js/main.ca29b0ad.chunk.js"
+  },
+  {
+    "revision": "b9de279fa8d9b9c74d05",
+    "url": "/cient-app/static/js/runtime-main.f56c1852.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cient-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5c0fbf301db7fafa8839a26d8d3fcaac",
+    "url": "/cient-app/static/media/profile.5c0fbf30.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-app/precache-manifest.6847c2283cec8d2096afa2413e019f60.js"
+  "/cient-app/precache-manifest.b1a2ccae16fe277e8abeb20128fec7b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cient-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
